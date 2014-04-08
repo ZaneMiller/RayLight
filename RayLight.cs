61e0f6d77deac1c71e683450882c71ca270213d5
unknown <designbyzane@gmail.com>
2014-04-07 18:21:52 -0700

    First commit. This commit comprises version 0.1 of the library.

---
+++ RayLight.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class RayLight : MonoBehaviour {
+	
+	public Material mat;
+	public float range;
+	public LayerMask layerMask;
+	
+	[HideInInspector]
+	public Vector3[] endpoints;
+	
+	
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.DrawWireSphere(transform.position, range);
+	}
+	
+	float GetAngle(Vector3 v)
+	{
+		Vector2 diff = v - transform.position;
+		return Mathf.Atan2(diff.y, diff.x);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+		//Get the unquie vertices of all ShadowCasters in range
+		List<Vector3> uniqueVertices = new List<Vector3>();
+		foreach(RayLightVertex rlv in FindObjectsOfType<RayLightVertex>())
+		{
+			if(rlv.useChildren)
+			{
+				foreach(Transform t in rlv.GetComponentInChildren<Transform>())
+				{
+					if(Vector2.Distance(transform.position, t.position) <= range)
+						uniqueVertices.Add(t.position);
+				}
+			}
+			else
+			{
+				if(Vector2.Distance(transform.position, rlv.transform.position) <= range)
+				{
+					uniqueVertices.Add(rlv.transform.position);
+				}
+			}
+
+		}
+		
+		float[] angles = new float[uniqueVertices.Count * 3];
+		
+		//Find all unique angles
+		int uniqueVertextCount = uniqueVertices.Count * 3;
+		for(int i = 3; i <= uniqueVertextCount; i += 3)
+		{
+			//We create angle offsets here to account of the edges of objects that light should move past.
+			//This also creates the correct ordering of vertexts for drawing.
+			float a = GetAngle(uniqueVertices[(i / 3) - 1]);
+			angles[i - 3] = a - 0.001f;
+			angles[i - 2] = a;
+			angles[i - 1] = a + 0.001f;
+		}
+		
+		//Sort the array by angle
+		Array.Sort<float> (angles);
+		
+		int angleLength = angles.Length;
+		
+		
+		endpoints = new Vector3[angleLength];
+		
+		//Find the endpoint of each light shaft
+		for(int i = 0; i < angleLength; i++)
+		{
+			Vector2 d = new Vector2(Mathf.Cos(angles[i]), Mathf.Sin(angles[i]));
+			RaycastHit2D rh = Physics2D.Raycast(transform.position, d, range, layerMask);
+			if(rh)
+				endpoints[i] = rh.point;
+			else
+				endpoints[i] = (Vector2)transform.position + range * d;
+		}
+	}
+}
---
+++ RayLightRenderer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayLightRenderer : MonoBehaviour {
+
+	public 
+
+	void Start()
+	{
+		Camera mc = Camera.main;
+		Camera c = GetComponent<Camera>();
+		c.aspect = mc.aspect;
+		c.orthographic = mc.orthographic;
+		c.orthographicSize = mc.orthographicSize;
+	}
+
+	//Draw dem triangles
+	void DrawTriangle(Vector3 v1, Vector3 v2, Vector3 v3, Material mat, float depth)
+	{
+		v1.z = depth;
+		v2.z = depth;
+		v3.z = depth;
+		GL.PushMatrix();
+		mat.SetPass(0);
+		GL.LoadOrtho();
+		GL.Color(mat.color);
+		GL.Begin(GL.TRIANGLES);
+		GL.Vertex(v1);
+		GL.Vertex(v2);
+		GL.Vertex(v3);
+		GL.End();
+		GL.PopMatrix();
+	}
+	
+	//Get dem triangles
+	void OnPostRender()
+	{
+		foreach(RayLight l in FindObjectsOfType<RayLight>())
+		{
+			Vector3[] endpoints = l.endpoints;
+			int len = endpoints.Length;
+			for(int i = 0; i < len; i++)
+			{
+				Vector3 next = i >= len - 1 ? endpoints[0] : endpoints[i + 1];
+				DrawTriangle(Camera.main.WorldToViewportPoint(l.transform.position), Camera.main.WorldToViewportPoint(endpoints[i]), Camera.main.WorldToViewportPoint(next), l.mat, l.transform.position.z);
+			}
+		}
+	}
+}
